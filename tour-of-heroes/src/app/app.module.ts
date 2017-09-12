2dbc93e64f645916c7acadec0d700fa90a1744b4
prajwalajayaprakash <prajwalaprakash09@gmail.com>
2017-09-12 01:44:28 -0400

    refactor: bind parent component to child component

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms"
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeroDetailComponent,
+   ],
   imports: [
     BrowserModule,
     FormsModule
