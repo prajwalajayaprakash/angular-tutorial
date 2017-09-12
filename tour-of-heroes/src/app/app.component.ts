d2fe54fc35f786b1b4425f7c441baad2b97cf7ea
prajwalajayaprakash <prajwalaprakash09@gmail.com>
2017-09-12 02:05:33 -0400

    feat: define hero service and use ngOnInit

--- tour-of-heroes/src/app/app.component.ts
+++ tour-of-heroes/src/app/app.component.ts
@@ -1,30 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
-const HEROES: Hero[] = [
-  {id: 11, name: 'Mr.Nice'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'},
-  {id: 15, name: 'Magneta'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr Iq'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-]
+
 @Component({
   selector: 'app-root',
   templateUrl:'./app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  heroes = HEROES;
+export class AppComponent implements OnInit{
+
+
+  constructor(private heroService: HeroService) {}
+  heroes: Hero[];
   title = 'Tour Of Heroes';
   selectedHero: Hero;
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
